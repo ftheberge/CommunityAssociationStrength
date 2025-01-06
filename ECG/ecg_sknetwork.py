365b3201b23390aa38f6068ca0e23a95a4ae8444
Ryan DeWolfe <ryandewolfe33@gmail.com>
2025-01-06 12:44:47 -0500

    Numba sknetwork outlier detection

--- ECG/ecg_sknetwork.py
+++ ECG/ecg_sknetwork.py
@@ -21,11 +21,11 @@ def is_internal_edge(indptr, indices, clusters):
 
 
 @jit
-def ief_to_cluster(indptr, indices, clusters, node, target_cluster, cluster_vols):
+def ief_to_cluster(indptr, indices, clusters, node, target_cluster, cluster_vols, eps=1e-6):
     neighbors = indices[indptr[node]:indptr[node+1]]
     deg = len(neighbors)
     in_com_deg = np.sum(clusters[neighbors] == target_cluster)
-    ief = in_com_deg/deg
+    ief = in_com_deg/(deg + eps)
     return ief
 
 
@@ -120,29 +120,99 @@ def cluster_edges(G, edge_weights, min_weight=0.05, twocore=True, final="leiden"
             WG.data[WG.indptr[i]:WG.indptr[i+1]] = min_weight
 
     if final == "leiden":
-            method = sn.clustering.Leiden(resolution=resolution)
+            method = sn.clustering.Leiden(resolution=resolution, shuffle_nodes=True)
     elif final == "louvain":
-            method = sn.clustering.Louvain(resolution=resolution)
+            method = sn.clustering.Louvain(resolution=resolution, shuffle_nodes=True)
     else:
             raise ValueError(f"final expected one of leiden or louvain. Got {final}")
     return method.fit_predict(WG)
 
 
-def outlier_scores(G, edge_weights, clustering):
-    degs = sn.utils.get_degrees(G)
-    good = np.zeros(len(degs), dtype="float32")
-    bad = np.zeros(len(degs), dtype="float32")
+#######################################
+# Outlier scores and pruning methods #
+######################################
+@jit
+def attatchment_scores(indptr, indices, edge_weights, clustering, eps=1e-6):
+    # 1 - outlier scores from ECG. So that low attatchment are likely outliers to match with CAS scores.
+    degs = np.array([indptr[i+1]-indptr[i] for i in range(len(indptr)-1)])
+    good = np.zeros(len(degs))
+    bad = np.zeros(len(degs))
     i = 0
-    for source in range(len(G.indptr)):
-        for target in G.indices[source:source+1]:
+    for source in range(len(indptr)-1):
+        for target in indices[indptr[source]:indptr[source+1]]:
             if clustering[source] == clustering[target]:
                 good[source] += edge_weights[i]
-                good[target] += edge_weights[i]
             else:
                 bad[source] += edge_weights[i]
-                bad[target] += edge_weights[i]
             i+=1
     
-    overall = (degs - bad - good)/degs
-    community = bad/(bad + good)
+    overall = 1 - (degs - bad - good)/(degs + eps)
+    community = 1 - bad/(bad + good + eps)
     return overall, community
+
+
+@jit
+def get_scores(indptr, indices, edge_weights, clusters, score):
+    scores = np.empty(len(indptr)-1, dtype="float64")
+    if score == "ief":
+        cluster_vols = get_cluster_vols(indptr, clusters)
+        for i in range(len(scores)):
+            scores[i] = ief_to_cluster(indptr, indices, clusters, i, clusters[i], cluster_vols)
+    elif score == "nief":
+        cluster_vols = get_cluster_vols(indptr, clusters)
+        for i in range(len(scores)):
+            scores[i] = ief_to_cluster(indptr, indices, clusters, i, clusters[i], cluster_vols)
+    elif score == "p":
+        cluster_vols = get_cluster_vols(indptr, clusters)
+        for i in range(len(scores)):
+            scores[i] = ief_to_cluster(indptr, indices, clusters, i, clusters[i], cluster_vols)
+
+    elif score in ["oout", "cout"]:
+        oout, cout = attatchment_scores(indptr, indices, edge_weights, clusters)
+        if score == "oout":
+            scores = oout
+        elif score == "cout":
+            scores = cout
+    return scores
+
+
+def prune(G, clusters, thresh, score="cout", max_per_round=100, recursive=True, edge_weights=None, eps=1e-6):
+    if score not in ["ief", "nief", "p", "oout", "cout"]:
+        raise ValueError(f"expected score to be one of: ief, nief, p, oout, cout. Got {score}.")
+    # Prune recursively. The graph is not reclustered, and the CAS scores are computed with edge weights of the original graph.
+    n = G.shape[0]
+    pruned = np.zeros(n, dtype="bool")
+
+    if score in ["oout", "cout"] and edge_weights is not None:
+        G = G.copy()  # don't update original
+        G.data = edge_weights
+    elif score in ["oout", "cout"]:
+        raise ValueError(f"edge_weights must be passed for score {score}")
+
+    done = False
+    round_counter = 1
+    while not done:
+        # Remove pruned nodes to make subgraph
+        index_map = np.arange(n)[~pruned]
+        subG = G[~pruned][:, ~pruned]
+        subclusters = clusters[~pruned]
+
+        scores = get_scores(subG.indptr, subG.indices, subG.data, subclusters, score)
+        # prune below thresh
+        argsort = np.argsort(scores)
+        if len(scores) > max_per_round and scores[argsort[max_per_round]] < thresh:
+            #prune max
+            to_prune = argsort[:max_per_round]
+            pruned[index_map[to_prune]] = True
+        elif len(scores) > 1 and scores[argsort[0]] < thresh:
+            # prune some
+            pruned[index_map[argsort[scores[argsort] < thresh]]] = True  # TODO ew
+        else:
+            # nothing to prune
+            done = True
+    
+        if not recursive:
+            done = True
+        round_counter += 1
+        #print(f"{np.sum(pruned)} nodes pruned after round {round_counter}")
+    return pruned
