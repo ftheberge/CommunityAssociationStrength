4357decde8ae88eb5232fe733813a00637608944
Ryan DeWolfe <ryandewolfe33@gmail.com>
2025-01-08 19:27:29 -0500

    numbaify ecg_igraph

--- ECG/ecg_igraph.py
+++ ECG/ecg_igraph.py
@@ -1,79 +1,138 @@
 import numpy as np
 import igraph as ig
-import scipy.sparse as sp
+#from scipy.stats import binom
 import sknetwork as sn
+
+from numba_stats import binom
+from numba import jit
 
 import sys
 sys.path.append('../')
 import CAS
 
 
-def compute_edge_weights(G, cas, clusters, combine_function="min", normalize=True):
-    source_to_target = np.empty(G.ecount())
-    target_to_source = np.empty(G.ecount())
-    for i, e in enumerate(G.es):
-        source_to_target[i] = cas[e.source, clusters[e.target]]
-        target_to_source[i] = cas[e.target, clusters[e.source]]
+EPS = 1e-6
 
-    if combine_function=="and":
-        weights = source_to_target * target_to_source
-    elif combine_function=="or":
-        weights = source_to_target + target_to_source - source_to_target*target_to_source
-    elif combine_function=="min":
-        weights = np.minimum(source_to_target, target_to_source)
-    elif combine_function=="mean":
-        weights = (source_to_target + target_to_source)/2
-    else:
-        raise ValueError(f"combine_function must be one of 'and', 'or', 'min', 'mean'. Got {combine_function}")
-    
-    if normalize:
-        return weights / (np.max(weights) - np.min(weights))
+
+@jit
+def is_internal_edge(indptr, indices, clusters):
+    is_internal = np.zeros(indptr[-1], dtype="bool")
+    i = 0
+    for source in range(len(indptr)-1):
+        for target in indices[indptr[source]:indptr[source+1]]:
+            is_internal[i] = clusters[source] == clusters[target]
+            i += 1
+    return is_internal
+
+
+@jit
+def ief_to_cluster(indptr, indices, clusters, node, target_cluster, cluster_vols, eps=1e-6):
+    neighbors = indices[indptr[node]:indptr[node+1]]
+    deg = len(neighbors)
+    in_com_deg = np.sum(clusters[neighbors] == target_cluster)
+    ief = in_com_deg/(deg + eps)
+    return ief
+
+
+@jit
+def nief_to_cluster(indptr, indices, clusters, node, target_cluster, cluster_vols):
+    nief = np.maximum(ief_to_cluster(indptr, indices, clusters, node, target_cluster, cluster_vols) - cluster_vols[target_cluster], 0.0)
+    return nief
+
+
+@jit
+def p_to_cluster(indptr, indices, clusters, node, target_cluster, cluster_vols):
+    neighbors = indices[indptr[node]:indptr[node+1]]
+    deg = np.array([len(neighbors)], dtype="int64")
+    in_com_deg = np.array([np.sum(clusters[neighbors] == target_cluster) - 1], dtype="float64")
+    p = binom._cdf(in_com_deg, deg, cluster_vols[target_cluster])[0]
+    return p
+
+
+@jit
+def ecg_to_cluster(indptr, indices, clusters, node, target_cluster, cluster_vols):
+    return int(clusters[node] == target_cluster)
+
+
+@jit
+def get_cluster_vols(indptr, clusters):
+    cluster_vols = np.zeros(np.max(clusters)+1, dtype="int32")
+    degrees = np.array([indptr[i+1]-indptr[i] for i in range(len(indptr)-1)])
+    for c, deg in zip(clusters, degrees):
+        cluster_vols[c] += deg
+    return cluster_vols / indptr[-1]  # indptr[-1] is the total volume of the graph
+
+
+@jit
+def compute_edge_weights(indptr, indices, clusters, cas):
+    weights = np.empty(len(indices), dtype="float64")
+    cluster_vols = get_cluster_vols(indptr, clusters)
+    i = 0
+    for source in range(len(indptr)-1):
+        for target in indices[indptr[source]:indptr[source+1]]:
+            weights[i] = cas(indptr, indices, clusters, source, clusters[target], cluster_vols)
+            i += 1
     return weights
 
 
-def cas_edge_weights(G, clusters, combine_function="min", normalize=True):
-    graph_matrix = G.get_adjacency_sparse()
-    cluster_matrix = CAS.partition2sparse(clusters)
-    ief, beta, c, p, degs = CAS.CAS(graph_matrix, cluster_matrix)
+@jit
+def get_edge_data_ids(edge_endpoint_ids, indptr, indices):
+    data_ids = np.empty_like(edge_endpoint_ids)
+    for i in range(edge_endpoint_ids.shape[0]):
+        source = edge_endpoint_ids[i, 0]
+        target = edge_endpoint_ids[i, 1]
+        for j, end in enumerate(indices[indptr[source]:indptr[source+1]]):
+            if end == target:
+                data_ids[i, 0] = indptr[source] + j
+                break
+        for j, end in enumerate(indices[indptr[target]:indptr[target+1]]):
+            if end == source:
+                data_ids[i, 1] = indptr[target] + j 
+    return data_ids
 
-    ief_weights = compute_edge_weights(G, ief, clusters, combine_function=combine_function, normalize=normalize)
-    beta_weights = compute_edge_weights(G, beta, clusters, combine_function=combine_function, normalize=normalize)
-    c_weights = compute_edge_weights(G, c, clusters, combine_function=combine_function, normalize=normalize)
-    p_weights = compute_edge_weights(G, p, clusters, combine_function=combine_function, normalize=normalize)
 
-    ecg_weights = compute_edge_weights(G, cluster_matrix, clusters, combine_function=combine_function, normalize=False)
-    
-    return ief_weights, beta_weights, c_weights, p_weights, ecg_weights
+@jit
+def add_weights(weights, edge_data_ids, data, combine):
+    for i in range(weights.shape[0]):
+        source_to_target = data[edge_data_ids[i, 0]]
+        target_to_source = data[edge_data_ids[i, 1]]
+        if combine == "and":
+            weights[i] += source_to_target * target_to_source
+        elif combine == "or":
+            weights[i] += source_to_target + target_to_source - source_to_target * target_to_source
+        elif combine == "min":
+            weights[i] += np.minimum(source_to_target, target_to_source)
+        elif combine == "mean":
+            weights[i] += (source_to_target + target_to_source)/2
 
 
-def ensemble_cas_edge_weights(G, ens_size=16, combine_function="min", normalize=True, clustering_method="first_louvain", resolution=1.0):
-    ief_weights = np.zeros(G.ecount())
-    beta_weights = np.zeros(G.ecount())
-    c_weights = np.zeros(G.ecount())
-    p_weights = np.zeros(G.ecount())
-    ecg_weights = np.zeros(G.ecount())
+def ensemble_edge_weights(g: ig.Graph, ens_size=16, cas=p_to_cluster, combine="and", method="first_louvain", resolution=1.0):
+    if combine not in ["and", "or", "min", "mean"]:
+        raise ValueError(f"combine_function expected one of and, or, min or mean. Got {combine}")
+    if method not in ["louvain", "first_louvain", "leiden", "first_leiden"]:
+        raise ValueError(f"clustering_method expected one of leiden, louvain, or first_louvain. Got {method}")
+
+    weights = np.zeros(g.ecount())
+    adj = g.get_adjacency_sparse()
+    edge_endpoint_ids = np.array([(e.source, e.target) for e in g.es])
+    edge_adj_data_ids = get_edge_data_ids(edge_endpoint_ids, adj.indptr, adj.indices)
 
     for _ in range(ens_size):
-        permutation = np.random.permutation(G.vcount())
-        g = G.permute_vertices(permutation)
-        if clustering_method == "leiden":
-            clustering = np.array(g.community_leiden(resolution=resolution).membership)
-        elif clustering_method == "louvain":
-            clustering = np.array(g.community_louvain(resolution=resolution).membership)
-        elif clustering_method == "first_louvain":
-            clustering = np.array(g.community_multilevel(return_levels=True)[0].membership)
-        else:
-            raise ValueError(f"clustering_method expected one of leiden, louvain, or first_louvain. Got {clustering_method}")
-        
-        ief, beta, c, p, ecg = cas_edge_weights(g, clustering, combine_function=combine_function, normalize=normalize)
+        permutation = np.random.permutation(g.vcount())
+        g_shuffled = g.permute_vertices(permutation)
+        if method == "leiden":
+            clusters = np.array(g_shuffled.community_leiden(resoltuion=resolution, objective_function="modularity").membership)
+        elif method == "first_leiden":
+            clusters = np.array(g_shuffled.community_leiden(resoltuion=resolution, objective_function="modularity", n_iterations=1).membership)
+        elif method == "louvain":
+            clusters = np.array(g_shuffled.community_multilevel(resoltuion=resolution).membership)
+        elif method == "first_louvain":
+            clusters = np.array(g_shuffled.community_multilevel(return_levels=True)[0].membership)
 
-        ief_weights += ief
-        beta_weights += beta
-        c_weights += c
-        p_weights += p
-        ecg_weights += ecg
+        clusters = clusters[permutation]
+        add_weights(weights, edge_adj_data_ids, compute_edge_weights(adj.indptr, adj.indices, clusters, cas), combine)
 
-    return ief_weights/ens_size, beta_weights/ens_size, c_weights/ens_size, p_weights/ens_size, ecg_weights/ens_size
+    return weights/ens_size
 
 
 def cluster_edges(G, edge_weights, min_weight=0.05, twocore=True, final="leiden", resolution=1.0):
@@ -108,24 +167,6 @@ def outlier_scores(G, edge_weights, clustering):
     overall = (degs - bad - good)/degs
     community = bad/(bad + good)
     return overall, community
-
-
-# Deprecated in favour of the lower version that uses sparse matrices.
-#def attatchment_scores(G, edge_weights, clustering):
-#    degs = np.array(G.degree())
-#    good = np.zeros(G.vcount())
-#    bad = np.zeros(G.vcount())
-#    for i, e in enumerate(G.es):
-#        if clustering[e.source] == clustering[e.target]:
-#            good[e.source] += edge_weights[i]
-#            good[e.target] += edge_weights[i]
-#        else:
-#            bad[e.source] += edge_weights[i]
-#            bad[e.target] += edge_weights[i]       
-#
-#    overall = 1 - (degs - bad - good)/degs
-#    community = 1 - bad/(bad + good)
-#    return overall, community
 
 
 ########################
