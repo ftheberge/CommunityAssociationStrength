85f9ae376974092c1a8c4559765ed71b7fcf59ae
Francois <theberge@ieee.org>
2025-04-14 07:53:43 -0400

    compare

--- ABCDoo/abcdoo_snap_graph_sampler.jl
+++ ABCDoo/abcdoo_snap_graph_sampler.jl
@@ -193,62 +193,53 @@ d_max_iter = 1000
 c_max_iter = 1000
 Random.seed!(seed)
 
-for params in [youtube_like_params]
+for params in [lj_noout_params]
     for d in [2]
         @info "$(params["name"]), d=$d"
 
         name = params["name"]
-        #n = params["n"]
+        n = params["n"]
         nout = params["nout"]
-        #η = params["η"]
+        η = params["η"]
         d_min = params["d_min"]
         d_max = params["d_max"]
         τ₁ = params["τ₁"]
         c_min = params["c_min"]
         c_max = params["c_max"]
         τ₂ = params["τ₂"]
-        #ξ = params["ξ"]
+        ξ = params["ξ"]
         ρ = params["ρ"]
 
-        for η in [1.25]
-            for ξ in [0.1]
-                for n in [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,11000,12000,13000,14000,15000]
-#         for η in [1.0,1.25,1.5,1.75,2.0,2.25,2.5]
-#             for ξ in [0.1,0.2,0.3,0.4,0.5,0.6]
-#                 for n in [5000]
 
-                    # in what follows n is number of non-outlier nodes
-                    n = n - nout
+        # in what follows n is number of non-outlier nodes
+        n = n - nout
 
-                    # Actually Generate the graph
-                    @info "Expected value of degree: $(ABCDGraphGenerator.get_ev(τ₁, d_min, d_max))"
-                    degs = ABCDGraphGenerator.sample_degrees(τ₁, d_min, d_max, n + nout, d_max_iter)
-                    @assert iseven(sum(degs))
+        # Actually Generate the graph
+        @info "Expected value of degree: $(ABCDGraphGenerator.get_ev(τ₁, d_min, d_max))"
+        degs = ABCDGraphGenerator.sample_degrees(τ₁, d_min, d_max, n + nout, d_max_iter)
+        @assert iseven(sum(degs))
 
 
-                    @info "Expected value of community size: $(ABCDGraphGenerator.get_ev(τ₂, c_min, c_max))"
-                    coms = ABCDGraphGenerator.sample_communities(τ₂, ceil(Int, c_min / η), floor(Int, c_max / η), n, c_max_iter)
-                    @assert sum(coms) == n
-                    pushfirst!(coms, nout)
+        @info "Expected value of community size: $(ABCDGraphGenerator.get_ev(τ₂, c_min, c_max))"
+        coms = ABCDGraphGenerator.sample_communities(τ₂, ceil(Int, c_min / η), floor(Int, c_max / η), n, c_max_iter)
+        @assert sum(coms) == n
+        pushfirst!(coms, nout)
 
-                    @info "    Done degs and coms, generating graph."
-                    p = ABCDGraphGenerator.ABCDParams(degs, coms, ξ, η, d, ρ)
-                    edges, clusters = ABCDGraphGenerator.gen_graph(p)
-                    open("abcdoo_$(name)_$(ξ)_$(η)_$(n)_edge.dat", "w") do io
-                    #open("abcdoo_$(name)_d$(d)_nocorr_edge.dat", "w") do io
-                       for (a, b) in sort!(collect(edges))
-                           println(io, a, "\t", b)
-                       end
-                    end
-                    open("abcdoo_$(name)_$(ξ)_$(η)_$(n)_com.dat", "w") do io
-                    #open("abcdoo_$(name)_d$(d)_nocorr_com.dat", "w") do io
-                        for (i, c) in enumerate(clusters)
-                            println(io, i, "\t", c)
-                        end
-                    end
-
-                end
+        @info "    Done degs and coms, generating graph."
+        p = ABCDGraphGenerator.ABCDParams(degs, coms, ξ, η, d, ρ)
+        edges, clusters = ABCDGraphGenerator.gen_graph(p)
+        open("abcdoo_$(name)_$(ξ)_$(η)_$(n)_edge.dat", "w") do io
+        #open("abcdoo_$(name)_d$(d)_nocorr_edge.dat", "w") do io
+           for (a, b) in sort!(collect(edges))
+               println(io, a, "\t", b)
+           end
+        end
+        open("abcdoo_$(name)_$(ξ)_$(η)_$(n)_com.dat", "w") do io
+        #open("abcdoo_$(name)_d$(d)_nocorr_com.dat", "w") do io
+            for (i, c) in enumerate(clusters)
+                println(io, i, "\t", c)
             end
         end
+
     end
 end
